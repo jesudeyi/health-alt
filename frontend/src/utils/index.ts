23e41fa712b80ac420be2df17f08b3b1ecd3b881
David Fadeyi <fadav4real@gmail.com>
2024-06-15 17:11:09 +0100

    feat: add commonly needed utility function

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,3 +1,7 @@
 export function classNames(...classes: unknown[]): string {
   return classes.filter(Boolean).join(' ')
 }
+
+export const capitalizeString = (str: string): string => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
