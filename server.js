e2bfc3bafbb751dad9f3618825f95ef88e049109
David Fadeyi <fadav4real@gmail.com>
2024-06-15 08:03:15 +0100

    feat: initialize backend

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import dotenv from "dotenv";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const initializeExpressServer = async () => {
+    dotenv.config();
+    const port = process.env.PORT;
+
+    const app = express();
+    app.use(express.json({ limit: "50mb" }));
+    app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+
+    if (process.env.NODE_ENV === "production") {
+        console.log("up here prod");
+        app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+        app.get("*", (req, res) =>
+            res.sendFile(
+                path.resolve(__dirname, "frontend", "dist", "index.html")
+            )
+        );
+    } else {
+        console.log("down here dev");
+        app.get("/", (req, res) => res.send("Please set to production"));
+    }
+
+    // Start the server
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+
+    return app;
+};
+
+const expressApp = initializeExpressServer();
+export default expressApp;
