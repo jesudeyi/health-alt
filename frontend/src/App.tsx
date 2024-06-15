14d3f7387e582f31d397c3d2d1e355c3f8cef401
David Fadeyi <fadav4real@gmail.com>
2024-06-15 16:01:43 +0100

    feat: add imports to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,9 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import './index.css'
+import 'aos/dist/aos.css'
+
 function App() {
   return <div>Hello World</div>
 }
