f67602b3d9c3b7e36de94c3a33fcfe0a4a221fee
David Fadeyi <fadav4real@gmail.com>
2024-06-15 17:13:48 +0100

    chore: configure vite proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,11 +5,25 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'happy-dom',
     setupFiles: '.vitest/setup',
     include: ['**/test.{ts,tsx}']
+  },
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
