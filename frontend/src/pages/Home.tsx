fcd351d79684ceca9dd1b9b58d45a7f633a15932
David Fadeyi <fadav4real@gmail.com>
2024-06-16 19:52:25 +0100

    feat: complete side menu implementation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, MouseEvent, useState } from 'react'
 import axios from 'axios'
 import { ClipLoader } from 'react-spinners'
 import { BsCamera } from 'react-icons/bs'
@@ -94,6 +94,7 @@ const Home = () => {
   )
   const [currentAlternativeIndex, setCurrentAlternativeIndex] =
     useState<number>(0)
+    const [rememberData, setRememberData] = useState<boolean>(false)
   const [formData, setFormData] = useState<FormData>({
     allergies: '',
     dietGoal: '',
@@ -276,6 +277,20 @@ const Home = () => {
     )
 
     setIsBuying(true)
+  }
+
+  const handleRememberDataChange = (
+    e: MouseEvent<HTMLButtonElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    e.preventDefault()
+    setRememberData(!rememberData)
+    localStorage.setItem('rememberData', JSON.stringify(!rememberData))
+
+    if (!rememberData) {
+      localStorage.setItem('formData', JSON.stringify(formData))
+    } else {
+      localStorage.removeItem('formData')
+    }
   }
 
   return (
@@ -614,7 +629,7 @@ const Home = () => {
 
       {/* Side menu */}
       <div
-        className={`flex flex-col justify-center fixed right-0 top-0 h-screen w-1/3 border-l-2 border-l-gray-700 shadow-black/30 px-6 shadow-lg transition-transform duration-300 dark:bg-gray-900 ${
+        className={`fixed right-0 top-0 flex h-screen w-1/3 flex-col justify-center border-l-2 border-l-gray-700 px-6 shadow-lg shadow-black/30 transition-transform duration-300 dark:bg-gray-900 ${
           sideMenuIsVisible ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
@@ -626,7 +641,168 @@ const Home = () => {
             <IoCloseSharp size={24} className="text-teal-500" />
           </button>
         </div>
-        <div className="px-4"></div>
+
+        <div className="px-4">
+          <div className="mb-4">
+            <label htmlFor="allergies" className="mb-2 block">
+              Allergies
+            </label>
+            <input
+              type="text"
+              name="allergies"
+              value={formData.allergies}
+              placeholder="Ex. Peanuts, Groundnut oil (comma-separated)"
+              className="w-full rounded-lg  border px-4 py-2 outline-none transition-colors ease-in-out placeholder:text-gray-300/40 focus:ring-2 focus:ring-teal-700 dark:border-gray-800 dark:bg-gray-800 dark:hover:border-teal-700"
+              onChange={handleFormChange}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="healthConditions" className="mb-2 block">
+              Health Conditions
+            </label>
+            <input
+              type="text"
+              name="healthConditions"
+              value={formData.healthConditions}
+              placeholder="Ex. Diabetes, Heart Disease (comma-separated)"
+              className="w-full rounded-lg  border px-4 py-2 outline-none transition-colors ease-in-out placeholder:text-gray-300/40 focus:ring-2 focus:ring-teal-700 dark:border-gray-800 dark:bg-gray-800 dark:hover:border-teal-700"
+              onChange={handleFormChange}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="dietaryPreference" className="mb-2 block">
+              Dietary Preference
+            </label>
+            <input
+              type="text"
+              name="dietaryPreference"
+              value={formData.dietaryPreference}
+              placeholder="Ex. Halal, Vegetarian, Vegan (comma-separated)"
+              className="w-full rounded-lg border px-4 py-2 outline-none transition-colors ease-in-out placeholder:text-gray-300/40 focus:ring-2 focus:ring-teal-700 dark:border-gray-800 dark:bg-gray-800 dark:hover:border-teal-700"
+              onChange={handleFormChange}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="fitnessLevel" className="mb-2 block">
+              Fitness Level
+            </label>
+            <div className="relative">
+              <button
+                className={`w-full rounded-md border px-4 py-2 text-left transition-colors ease-in-out focus:ring-2 focus:ring-teal-700 dark:border-gray-800 dark:bg-gray-800 dark:hover:border-teal-700 ${
+                  !formData.fitnessLevel ? ' text-gray-300/40' : ''
+                }`}
+                onClick={() => {
+                  setFitnessLevelDropdownOpen(!fitnessLevelDropdownOpen)
+                }}
+              >
+                {capitalizeString(formData.fitnessLevel) ||
+                  'Select Fitness Level'}
+              </button>
+              <div className="absolute left-[90%] top-[0.55rem] w-full">
+                <IoChevronDown size={24} className="text-teal-500" />
+              </div>
+              {fitnessLevelDropdownOpen && (
+                <div className="absolute left-0 top-14 w-full rounded-md border border-gray-300 bg-white shadow-md ring-2 ring-teal-700 dark:border-teal-700 dark:bg-gray-800">
+                  <button
+                    className={`w-full rounded-md px-4 py-2 hover:rounded-none hover:bg-gray-300 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-teal-700`}
+                    onClick={() => handleFitnessLevelChange('sedentary')}
+                  >
+                    Sedentary (Little or no exercise)
+                  </button>
+                  <button
+                    className={`w-full rounded-md px-4 py-2 hover:rounded-none hover:bg-gray-300 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-teal-700`}
+                    onClick={() => handleFitnessLevelChange('moderate')}
+                  >
+                    Moderate (Exercise 3-5 days/week)
+                  </button>
+                  <button
+                    className={`w-full rounded-md px-4 py-2 hover:rounded-none hover:bg-gray-300 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-teal-700`}
+                    onClick={() => handleFitnessLevelChange('active')}
+                  >
+                    Active (Exercise 6-7 days/week)
+                  </button>
+                  <button
+                    className={`w-full rounded-md px-4 py-2 hover:rounded-none hover:bg-gray-300 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-teal-700`}
+                    onClick={() => handleFitnessLevelChange('veryActive')}
+                  >
+                    Very Active (Exercise multiple times per day)
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="dietGoal" className="mb-2 block">
+              Diet Goal
+            </label>
+            <input
+              type="text"
+              name="dietGoal"
+              value={formData.dietGoal}
+              placeholder="Enter your diet goal"
+              className="w-full rounded-lg  border px-4 py-2 outline-none transition-colors ease-in-out placeholder:text-gray-300/40 focus:ring-2 focus:ring-teal-700 dark:border-gray-800 dark:bg-gray-800 dark:hover:border-teal-700"
+              onChange={handleFormChange}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="lifeStage" className="mb-2 block">
+              Life Stage
+            </label>
+            <input
+              type="text"
+              name="lifeStage"
+              value={formData.lifeStage}
+              placeholder="Ex. Pregnancy, Elderly, Menstrual Period, Nursing"
+              className="w-full rounded-lg  border px-4 py-2 outline-none transition-colors ease-in-out placeholder:text-gray-300/40 focus:ring-2 focus:ring-teal-700 dark:border-gray-800 dark:bg-gray-800 dark:hover:border-teal-700"
+              onChange={handleFormChange}
+            />
+          </div>
+
+          <div className="space-y-1">
+            <p className="text-center text-xs text-gray-300/80">
+              Your data is safe and only stored on your device
+            </p>
+            <div className="flex items-center justify-center">
+              <input
+                type="checkbox"
+                id="rememberData"
+                name="rememberData"
+                checked={rememberData}
+                onChange={handleRememberDataChange}
+                className="sr-only mr-2" // Hide the default checkbox
+              />
+              <label
+                htmlFor="rememberData"
+                className={`relative mr-2 size-4 cursor-pointer rounded-[0.18rem] border border-teal-500 ${
+                  rememberData ? 'bg-teal-500' : 'bg-transparent'
+                }`}
+              >
+                {rememberData && (
+                  <svg
+                    className="absolute left-1/2 top-1/2 w-4 -translate-x-1/2 -translate-y-1/2 text-white"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M9 16.2l-4.6-4.6 1.4-1.4 3.2 3.2 7.2-7.2 1.4 1.4-8.6 8.6z"
+                    />
+                  </svg>
+                )}
+              </label>
+              <button
+                onClick={handleRememberDataChange}
+                className="text-teal-400"
+              >
+                Remember my data
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
