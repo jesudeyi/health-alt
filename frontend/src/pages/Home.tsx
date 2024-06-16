c98d6f4b456c97d8bbaf596c0a4b1864f1e1c245
David Fadeyi <fadav4real@gmail.com>
2024-06-16 16:42:43 +0100

    feat: add page structure

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className="relative flex flex-col items-start justify-center overflow-y-scroll">
+      <div>
+        {/* A. Topmost section */}
+        <div className="mb-12 flex items-center justify-between px-4 "></div>
+
+        {/* B. Next section */}
+        <div></div>
+      </div>
+
+      {/* Side menu */}
+      <div></div>
+    </div>
+  )
 }
 
 export default Home
